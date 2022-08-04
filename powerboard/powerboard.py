14b8a72c0bd002db5d146281022e477a106d8e4b
danlsn <danlsn@github.com>
2022-08-04 16:24:02 +1000

    Feat: create amber electric api instances

--- powerboard/powerboard.py
+++ powerboard/powerboard.py
@@ -1,6 +1,21 @@
 import configparser
+import amberelectric
+from amberelectric.api import amber_api
+
 
 # Safely Import Credentials Using 'configparser'
-CONFIG_FILE = '../config.ini'
+CONFIG_FILE = "../config.ini"
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
+
+# Amber Electric API Configuration
+amber_u1_config = amberelectric.Configuration(
+    access_token=config["AMBER_UNIT1"]["TOKEN"]
+)
+amber_u2_config = amberelectric.Configuration(
+    access_token=config["AMBER_UNIT2"]["TOKEN"]
+)
+
+# Create Amber Electric API Instances
+amber_u1 = amber_api.AmberApi.create(amber_u1_config)
+amber_u2 = amber_api.AmberApi.create(amber_u2_config)
